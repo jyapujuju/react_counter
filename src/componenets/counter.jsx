42fca03207c2f7d373265b199bd9b7936bec8559
jyapujuju <danieldangol@gmail.com>
2018-09-17 14:26:10 +0545

    buggy

---
+++ src/componenets/counter.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  handleIncreament = product => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  render() {
+    let classes = this.getBadgeClasses();
+    return (
+      <React.Fragment>
+        <span className={classes}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncreament(product)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
